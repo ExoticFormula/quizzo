16ad4fbf2978abd7f7a7950e56f13ff168ba4d78
Kishor Krishna <kishorkrishna3k@gmail.com>
2022-10-14 13:26:35 +0530

    added timer for questions

--- app.js
+++ app.js
@@ -6,81 +6,87 @@ let attendedquestionscount = 1;
 let score = 0;
 let questions = [
   {
-    question: "Who is the founder Of Microsoft?",
-    image: "https://wallpaperaccess.com/full/833268.png",
-    options: ["Alan Turing", "Sabeer Bhatia", "Jeff Bezoz", "Bill Gates"],
-    answer: "Bill Gates",
-  },
-  {
-    question: "What is the full form of USB?",
+    question: "Who created Java programming language?",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/SanDisk-Cruzer-USB-4GB-ThumbDrive.jpg/1200px-SanDisk-Cruzer-USB-4GB-ThumbDrive.jpg",
+      "https://images.idgesg.net/images/article/2019/05/java_binary_code_gears_programming_coding_development_by_bluebay2014_gettyimages-1040871468_2400x1600-100795798-large.jpg?auto=webp&quality=85,70",
     options: [
-      "Uniform Serial Bus",
-      "Universal Serial Bus",
-      "Uniform State Bus",
-      "Uniform Serial Buffer",
+      "James Gosling",
+      "Richard Stallman",
+      "Guido Van Rossum",
+      "Bjarne Stroustrup",
     ],
-    answer: "Universal Serial Bus",
+    answer: "James Gosling",
   },
   {
-    question: "Which company built the first smartphone?",
-    image: "https://simpletexting.com/wp-content/uploads/2020/04/ibm_simon.jpg",
-    options: ["Google", "Samsung", "IBM", "Nokia"],
-    answer: "IBM",
-  },
-  {
-    question: "Which is the first ever web browser?",
-    image:
-      "https://msatechnosoft.in/blog/wp-content/uploads/2017/04/top-web-browsers-MSA-Technosoft.png",
-    options: ["Nexus", "Mosaic", "Internet Explorer", "Mozilla"],
-    answer: "Nexus",
-  },
-  {
-    question: "First computer virus is known as",
-    image:
-      "https://www.lifewire.com/thmb/_twpha56FPU3mJIsTs6bS2Ytfrg=/1920x1200/filters:fill(auto,1)/what-is-a-computer-virus-a82f9491ad3644b89446d45233b57761.jpg",
-    options: ["Rabbit", "Creeper Virus", "Elk Cloner", "SCA Virus"],
-    answer: "Creeper Virus",
-  },
-  {
-    question: "Computer Hard Disk was first introduced in 1956 by",
-    image:
-      "https://images.wondershare.com/recoverit/article/2019/06/hard-drive-plates.jpg",
-    options: ["Dell", "IBM", "Apple", "Microsoft"],
-    answer: "IBM",
-  },
-  {
-    question: "Which protocol is used to receive e-mail?",
-    image:
-      "https://images.ctfassets.net/lzny33ho1g45/best-email-app-p-img/65fb438385539afec1566a61ffbf7668/best_email_apps.jpg?w=1520&fm=jpg&q=30&fit=thumb&h=760",
-    options: ["SMTP", "POP3", "HTTP", "FTP"],
-    answer: "POP3",
-  },
-  {
-    question: "Who is the founder Of Bluetooth?",
-    image:
-      "https://9to5mac.com/wp-content/uploads/sites/6/2017/02/bluetooth-logo-e1546276877986.png?w=1600",
-    options: ["IBM", "Dell", "Apple", "Ericsson"],
-    answer: "Ericsson",
-  },
-  {
-    question: "Who is known as the father of indian Supercomputing?",
-    image:
-      "https://www.tata.com/content/dam/tata/images/newsroom/heritage/desktop/eka_banner_desktop_1920x1080.jpg",
+    question:
+      "Which protocol is used to identify the hardware address of a local device?",
+    image: "https://devqa.io/assets/images/networking-protocol-basics.png",
     options: [
-      "Ragunath Mashelkar",
-      "Vijay Bhatkar",
-      "Jayant",
-      "Nandan Nilekani",
+      "Address Resolution Protocol",
+      "Media Access Protocol",
+      "Simple Address Transfer Protocol",
+      "Tranmission Control Protocol",
     ],
-    answer: "Vijay Bhatkar",
+    answer: "Address Resolution Protocol",
   },
   {
-    question: "Which country does this flag belong to?",
-    image: "https://cdn.britannica.com/69/5869-004-7D75CD05/Flag-Argentina.jpg",
-    options: ["Argentina", "India", "Bhutan", "Lebanon"],
-    answer: "Argentina",
+    question: "Twitter is an example of what service?",
+    image: "https://www.punekarnews.in/wp-content/uploads/2021/03/Twitter.jpg",
+    options: ["Blog", "Micro Blog", "Forum", "Gallery"],
+    answer: "Micro Blog",
+  },
+  {
+    question: "Who is the father of World Wide Web(WWW)?",
+    image:
+      "https://cdn.britannica.com/73/78373-050-2D15D41C/Tim-Berners-Lee.jpg",
+    options: [
+      "Dennis Ritchie",
+      "John Mccarthy",
+      "Michael Dell",
+      "Tim Berners Lee",
+    ],
+    answer: "Tim Berners Lee",
+  },
+  {
+    question: " When was the term Social Networking first used?",
+    image:
+      "https://clockwise.software/img/blog/how-to-build-a-social-network-website/header-background.png",
+    options: ["1960", "1954", "1980", "2000"],
+    answer: "1954",
+  },
+  {
+    question: "Which is known as the programming language of Web?",
+    image:
+      "https://www.simplilearn.com/ice9/free_resources_article_thumb/Best-Programming-Languages-to-Start-Learning-Today.jpg",
+    options: ["Java", "PHP", "Javascript", "Python"],
+    answer: "Javascript",
+  },
+  {
+    question: "What do you call a single point on a computer screen?",
+    image: "https://i.ytimg.com/vi/AjFXlj0WmRs/maxresdefault.jpg",
+    options: ["Pixel", "Density", "Vertex", "Resolution"],
+    answer: "Pixel",
+  },
+  {
+    question:
+      "What is part of a database that holds only one type of information?",
+    image:
+      "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777046/0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Social-754bc73011e057dc76e55a44a954e0c3.png",
+    options: ["Report", "Record", "Field", "File"],
+    answer: "Field",
+  },
+  {
+    question: "A folder in windows cannot be made with the name",
+    image: "https://www.wintips.org/wp-content/uploads/2017/12/image-15.png",
+    options: ["can", "con", "tmp", "mak"],
+    answer: "con",
+  },
+  {
+    question: "What is SATA?",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6eHDirt42Dr80zExM3EF6boFfXNA9dbV2lg&usqp=CAU",
+    options: ["Storage Device", "Virus", "Anti-Virus", "Hard Drive Interface"],
+    answer: "Hard Drive Interface",
   },
 ];
 const startbutton = document.querySelector(".btn-start");
@@ -112,6 +118,27 @@ const changeQuestion = (q, index) => {
   let progressbar = document.createElement("p");
   progressbar.innerText = attendedquestionscount + "/10";
   questioncontainer.appendChild(progressbar);
+  let timermax = 8;
+
+  let timer = document.createElement("span");
+  timer.innerText = `Time Left: ${timermax}`;
+  questioncontainer.appendChild(timer);
+
+  let timerinterval = setInterval(() => {
+    timermax--;
+    timer.innerText = `Time Left: ${timermax}`;
+
+    if (timermax == 0) {
+      attendedquestionscount++;
+
+      if (questionindex == 9) {
+        displayScore(playername, score);
+      } else {
+        changeQuestion(questions[questionindex + 1], questionindex + 1);
+      }
+    }
+  }, 1000);
+
   let optionscontainer = document.createElement("div");
   optionscontainer.classList.add("options-container");
   let optionsform = document.createElement("form");
@@ -130,7 +157,6 @@ const changeQuestion = (q, index) => {
     option.addEventListener("click", () => {
       checkedoption = option;
       document.querySelectorAll(".option").forEach((optionitem) => {
-        console.log(optionitem == option && !optionitem.firstChild.checked);
         if (optionitem == option && !optionitem.firstChild.checked) {
           optionitem.firstChild.checked = true;
         }
@@ -145,6 +171,7 @@ const changeQuestion = (q, index) => {
   btnanswer.innerText = questionindex == 9 ? "FINISH" : "SUBMIT";
   btnanswer.addEventListener("click", (e) => {
     e.preventDefault();
+    clearInterval(timerinterval);
     if (btnanswer.innerText === "NEXT") {
       attendedquestionscount++;
       progressbar.innerText = attendedquestionscount + "/10";
@@ -173,7 +200,7 @@ const changeQuestion = (q, index) => {
         }
       }
 
-      if (attendedquestionscount == 10) {
+      if (questionindex == 9) {
         displayScore(playername, score);
       }
     }
@@ -185,25 +212,17 @@ const changeQuestion = (q, index) => {
   document.body.appendChild(questioncontainer);
 };
 const displayScore = (playername, score) => {
-  emailjs
-    .send("service_2exblmo", "template_8pkay8x", {
-      message: `Name: ${playername}, Score: ${score}`,
-    })
-    .then(
-      (response) => {
-        return;
-      },
-      (error) => {
-        return;
-      }
-    );
+  emailjs.send("service_2exblmo", "template_8pkay8x", {
+    message: `Name: ${playername}, Score: ${score}`,
+  });
+
   const questioncontainer = document.querySelector(".question-container");
   if (questioncontainer) questioncontainer.remove();
   const resultscontainer = document.createElement("div");
   resultscontainer.classList.add("container", "results-container");
-  console.log(resultscontainer);
   const congratstext = document.createElement("h2");
-  if (score >= 7) congratstext.innerText = `Congratulations ${playername} !!`;
+  if (score >= 7)
+    congratstext.innerText = `Congratulations :D ${playername} !!`;
   else if (score >= 5) congratstext.innerText = `That's good, ${playername} !!`;
   else if (score >= 3) congratstext.innerText = `Not too bad, ${playername}`;
   else congratstext.innerText = `Better luck next time :(, ${playername} `;
